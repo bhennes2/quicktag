1914ad1ff266193aeaa30ef0a1c0126a0f833a22
Brendan Hennessy <bhennes2@gmail.com>
2012-02-04 17:00:49 -0600

    fix routing issue on pages

--- config/routes.rb
+++ config/routes.rb
@@ -53,7 +53,8 @@ Quicktag::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-
+  match '/users' => 'home#users', :as => :users
+  match '/tags' => 'home#tags', :as => :tags	
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
