1bf3a7d64e6bdf51dc3ea4b2912bb73d996fe6e8
Brendan Hennessy <bhennes2@gmail.com>
2012-02-06 16:35:11 -0600

    added authentication

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,16 @@
 class HomeController < ApplicationController
+  before_filter :authenticate
+  
   def index
+  
   end
-
+  
+  protected
+  
+  def authenticate
+  	authenticate_or_request_with_http_basic do |username, password|
+		username == "admin" && password == "admin"
+  	end 
+  end
+  
 end
